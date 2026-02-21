eaa47ccdce8ba5c026ef7872183d7a9debc70ac7
Gabriel Everton Alves de Lima <gabrielacademico55@gmail.com>
2026-02-21 15:37:15 -0300

    Fix route path

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.get("/", (req, res) => {
+app.get("*", (req, res) => {
   res.status(200).json({ message: "Servidor online 🚀" })
 })
 
